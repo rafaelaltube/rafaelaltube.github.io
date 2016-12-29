a86053cd54b8f2d7cef338723e62157fbba418da
rafaelaltube <rafael.altubevera@googlemail.com>
2016-12-29 12:20:14 +0100

    ES/EN

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php header("Location: ./es/index.html"); ?>
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+<link rel="icon" type="image/png" href="favicon.png">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+<title>Rafael Altube</title>
+
+<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+
+
+</head>
+
+<body class="profile-page">
+
+
+
+</body>
+<script type="text/javascript">
+	window.console.log("%c%s","color: red; background: yellow; font-size: 24px;","ADVERTENCIA");
+	window.console.log("%c%s","color: black; font-size: 18px;","Si utilizas esta consola, otras personas podr\u00edan hacerse pasar por ti y robarte datos mediante un ataque Self-XSS.\nNo escribas ni pegues ning\u00fan c\u00f3digo que no entiendas.");
+</script>
+
+
+
+
+
+</html>
